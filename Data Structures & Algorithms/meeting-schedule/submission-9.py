022f2870bd10b04f38417eb715c6da9ec51de9c2
sow-2910 <ITCSIU24068@student.hcmiu.edu.vn>
2026-09-08 12:36:27 +0700

    Add: meeting-schedule - submission-9

---
+++ Data Structures & Algorithms/meeting-schedule/submission-9.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+
+
+        if not intervals:
+            return True
+        intervals.sort(key = lambda i:i.start)
+        prevEnd = intervals[0].end
+
+        for i in intervals[1:]:
+            if i.start >= prevEnd:
+                prevEnd = i.end
+            else:
+                return False
+
+        return True
