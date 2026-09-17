c777b8d164a9824a1f5211fb1f039f82a14d5c18
sow-2910 <ITCSIU24068@student.hcmiu.edu.vn>
2026-09-17 09:04:38 +0700

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map <Character, Integer> hashMap = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            hashMap.put(c, 1 + hashMap.getOrDefault(c, 0));
+        }
+
+        int currentSize = 0;
+        List<Integer> result = new ArrayList<>();
+        HashSet<Character> hashSet = new HashSet<>();
+
+        for (char c : s.toCharArray()){
+            hashSet.add(c);
+            hashMap.put(c, hashMap.get(c) - 1);
+            currentSize++;
+
+            if(hashMap.get(c) == 0){
+                hashSet.remove(c);
+            }
+
+            if(hashSet.isEmpty()){
+                result.add(currentSize);
+                currentSize = 0;
+            }
+
+        }
+        return result;
+    }
+}
