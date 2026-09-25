b8fdec3c68ea5c9873c186bdb3fa0a9a83ce848a
sow-2910 <ITCSIU24068@student.hcmiu.edu.vn>
2026-09-25 14:21:42 +0700

    Add: islands-and-treasure - submission-6

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        q = deque()
+        visited = set()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    q.append([r, c])
+                    visited.add((r, c))
+
+        def addCell(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == -1 or (r,c) in visited):
+                return
+            
+            visited.add((r,c))
+            q.append([r,c])
+
+
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = dist
+                addCell(r + 1, c)
+                addCell(r - 1, c)
+                addCell(r, c + 1)
+                addCell(r, c - 1)
+
+            dist += 1
+
